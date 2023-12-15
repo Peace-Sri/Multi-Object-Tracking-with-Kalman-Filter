eb3b18acf3f8435d4436a2a9a8ec46364b979f16
ThaiteamRyowa <thaiteam.ryowa@gmail.com>
2023-12-15 11:42:55 +0900

    Write as module

--- object_track.py
+++ object_track.py
@@ -1,8 +1,9 @@
 import numpy as np 
 import cv2
-from tracker import Tracker
+from tracker.tracker import Tracker
 import time
-import imageio
+#import imageio
+import random
 images = []
 
 def createimage(w,h):
@@ -11,39 +12,65 @@ def createimage(w,h):
 	return img
 
 def main():
-	data = np.array(np.load('Detections.npy'))[0:10,0:150,0:150]
+	data = np.array(np.load('Detections.npy'))[0:15,0:300000,0:300000]
 	tracker = Tracker(150, 30, 5)
 	skip_frame_count = 0
 	track_colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0),
 					(127, 127, 255), (255, 0, 255), (255, 127, 255),
 					(127, 0, 255), (127, 0, 127),(127, 10, 255), (0,255, 127)]
 
-	for i in range(data.shape[1]):
-		centers = data[:,i,:]
+	out = cv2.VideoWriter('result.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 30, (512,512))
+	del_test = [5, 6]
+	del_test_range = 60
+	circles = [5,11]
+	test_range = [20,40]
+	for i in range(data.shape[1]): #data.shape[1]
+		if (i >= test_range[0] and i < test_range[1]):
+			centers = data[:,i,:]
+			centers  = centers[circles[0]:circles[1],:]
+			#centers = np.array([])
+		else:
+			centers = data[:,i,:]
 		frame = createimage(512,512)
-		if (len(centers) > 0):
-			tracker.update(centers)
-			for j in range(len(tracker.tracks)):
-				if (len(tracker.tracks[j].trace) > 1):
-					x = int(tracker.tracks[j].trace[-1][0,0])
-					y = int(tracker.tracks[j].trace[-1][0,1])
-					tl = (x-10,y-10)
-					br = (x+10,y+10)
-					cv2.rectangle(frame,tl,br,track_colors[j],1)
-					cv2.putText(frame,str(tracker.tracks[j].trackId), (x-10,y-20),0, 0.5, track_colors[j],2)
-					for k in range(len(tracker.tracks[j].trace)):
-						x = int(tracker.tracks[j].trace[k][0,0])
-						y = int(tracker.tracks[j].trace[k][0,1])
-						cv2.circle(frame,(x,y), 3, track_colors[j],-1)
-					cv2.circle(frame,(x,y), 6, track_colors[j],-1)
-				cv2.circle(frame,(int(data[j,i,0]),int(data[j,i,1])), 6, (0,0,0),-1)
-			cv2.imshow('image',frame)
-			# cv2.imwrite("image"+str(i)+".jpg", frame)
-			# images.append(imageio.imread("image"+str(i)+".jpg"))
-			time.sleep(0.1)
-			if cv2.waitKey(1) & 0xFF == ord('q'):
-				cv2.destroyAllWindows()
-				break
+		predict_member = []
+		#if (len(centers) > 0):
+		
+		if i == del_test_range:
+			tracker.remove(del_test)
+		tracker.predict() #Predict State
+		for j in range(len(tracker.tracks)): #Get Length of member tracking | for j in range(len(tracker.tracks))
+			if (len(tracker.tracks[j].trace) > 1): #Check trace queue of tracking object
+				x = int(tracker.tracks[j].trace[-1][0,0]) #Get Predicted X from Kalman
+				y = int(tracker.tracks[j].trace[-1][0,1]) #Get Predicted Y from Kalman
+				tl = (x-10,y-10) # X min, Y min
+				br = (x+10,y+10) # X max, Y max
+				predict_member.append([x,y])
+				cv2.rectangle(frame,tl,br,track_colors[j],1)
+				cv2.putText(frame,"TrackID" + str(tracker.tracks[j].trackId), (x-10,y-20),0, 0.5, track_colors[j],2)
+				"""
+				for k in range(len(tracker.tracks[j].trace)):
+					x = int(tracker.tracks[j].trace[k][0,0])
+					y = int(tracker.tracks[j].trace[k][0,1])
+					cv2.circle(frame,(x,y), 3, track_colors[j],-1)
+				cv2.circle(frame,(x,y), 6, track_colors[j],-1)
+				"""
+			if len(centers) > 0:
+				if i > test_range[0] and i < test_range[1]:
+					for circle in range(circles[0],circles[1]):
+						cv2.circle(frame,(int(data[circle ,i,0]),int(data[circle,i,1])), 6, (0,0,0),-1)
+				else:
+					cv2.circle(frame,(int(data[j ,i,0]),int(data[j,i,1])), 6, (0,0,0),-1)
+
+		cv2.imshow('image',frame)
+		tracker.update(centers) #Update State
+		out.write(frame)
+		# cv2.imwrite("image"+str(i)+".jpg", frame)
+		# images.append(imageio.imread("image"+str(i)+".jpg"))
+		time.sleep(0.1)
+		#cv2.waitKey(0)
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			cv2.destroyAllWindows()
+			break
 
 	# imageio.mimsave('Multi-Object-Tracking.gif', images, duration=0.08)
 			
---
+++ tracker/tracker.py
@@ -0,0 +1,244 @@
+import numpy as np 
+from tracker.kalmanFilter import KalmanFilter
+from scipy.optimize import linear_sum_assignment
+from collections import deque
+
+
+class Tracks(object):
+	"""docstring for Tracks"""
+	def __init__(self, detection, trackId):
+		super(Tracks, self).__init__()
+		self.KF = KalmanFilter() #Init all A,B,
+		self.KF.predict()
+		self.KF.correct(np.matrix(detection).reshape(2,1))
+		self.trace = deque(maxlen=20)
+		self.prediction = detection.reshape(1,2)
+		self.trackId = trackId
+		self.skipped_frames = 0
+
+	def predict(self):
+		self.prediction = np.array(self.KF.predict()).reshape(1,2)
+	def update(self,detection):
+		self.KF.correct(np.matrix(detection).reshape(2,1))
+
+
+class Tracker(object):
+	"""docstring for Tracker"""
+	def __init__(self, dist_threshold=150, max_frame_skipped=30, max_trace_length=5):
+		super(Tracker, self).__init__()
+		self.dist_threshold = dist_threshold
+		self.max_frame_skipped = max_frame_skipped
+		self.max_trace_length = max_trace_length
+		self.trackId = 0
+		self.tracks = []
+		self.previous_index_tracks = 0
+	
+	def remove(self, del_tracks):
+		del_tracks.sort(reverse=True) # To Remove from last to front because if we dont use reverse it will cause out of length
+		for i in del_tracks:
+			print(f"Deleting Track: {i}")
+			del self.tracks[i] # Assume self.tracks = [track0, track1, track2, track3, track4, track5] del self.track[0] @ i=1 ->  [track1, track2, track3, track4, track5] 
+		self.previous_index_tracks = len(self.tracks) # Update previous_index_tracks
+
+		
+	
+	def predict(self):
+		if len(self.tracks) != 0:
+			for i in range(len(self.tracks)):
+				self.tracks[i].predict() #Update predict value Xp(PredictedState), Pp(errorCovraiance)
+				self.tracks[i].trace.append(self.tracks[i].prediction)
+	
+	def update(self, detections):
+		if (detections.shape[0] >= self.previous_index_tracks) and (detections.shape[0] != 0) : #Check if index of tracks are greater or equal than previous one
+			if len(self.tracks) == 0: #Check if first time running
+				for i in range(detections.shape[0]): #Loop to get all detections
+					track = Tracks(detections[i], self.trackId) #Init Tracks from 0 to n
+					self.tracks.append(track) #Add init track to self.tracks
+					self.trackId += 1 #Update trackID for preventing same ID used
+
+			N = len(self.tracks) #Length of self.tracks
+			M = len(detections) #Length of detections
+			cost = [] # Cost
+			""" Calculate Minimum Assignments """
+			for i in range(N):
+				diff = np.linalg.norm(self.tracks[i].prediction - detections.reshape(-1,2), axis=1) # Use Normalization 2 for compare Prediction@[k-1] and Real
+				cost.append(diff)
+			""" ############################# """
+				
+			cost = np.array(cost)*0.1
+			row, col = linear_sum_assignment(cost) # Minimum Assignment row = [0,1,2,3,n], col = [ass0,ass1,ass2,assn]
+			assignment = [-1]*N # Initial null assignments
+			for i in range(len(row)):
+				assignment[row[i]] = col[i] # Assigned all track
+
+
+			un_assigned_tracks = []
+
+			for i in range(len(assignment)): #Loop for finding un_assignments
+				if assignment[i] != -1: # Check If assignments has been assigned
+					if (cost[i][assignment[i]] > self.dist_threshold): # If assigned track is exceeding to distance threshold, It will be declared to unassigned track
+						assignment[i] = -1 # Assign which track to -1
+						un_assigned_tracks.append(i) # Append to un_assigned_tracks
+					else:
+						self.tracks[i].skipped_frames +=1 #If frame has been skipped
+
+
+			del_tracks = []
+			for i in range(len(self.tracks)):
+				if self.tracks[i].skipped_frames > self.max_frame_skipped:
+					print(f"Skipped: {self.tracks[i].skipped_frames}")
+					del_tracks.append(i)
+
+			if len(del_tracks) > 0:
+				for i in range(len(del_tracks)):
+					del self.tracks[i]
+					del assignment[i]
+
+			#If new track has been added more please uncomment this
+			for i in range(len(detections)): 
+				if i not in assignment:
+					track = Tracks(detections[i], self.trackId)
+					self.tracks.append(track)
+					self.trackId +=1
+
+			for i in range(len(assignment)):
+				if(assignment[i] != -1):
+					self.tracks[i].skipped_frames = 0
+					self.tracks[i].update(detections[assignment[i]]) # Update State
+
+			""" Update Previous equals current index tracks """
+			self.previous_index_tracks = detections.shape[0]
+
+		elif (detections.shape[0] < self.previous_index_tracks) : #Check if index of tracks are greater or equal than previous one
+			if detections.shape[0] > 0:
+				N = len(self.tracks)
+				M = len(detections)
+				cost = []
+
+				preds_all = []
+				for i in range(N):
+					preds_all.append(self.tracks[i].prediction[0])
+
+				for i in range(M):
+					diff = np.linalg.norm(preds_all - detections[i], axis=1) # Use Normalization 2 for compare Prediction@[k-1] and Real
+					cost.append(diff)
+				cost = np.array(cost)*0.1
+				row, col = linear_sum_assignment(cost) # Minimum Assignment
+				assignment = [-1] * len(col)
+				i = 0
+
+				un_assigned_tracks = []
+
+				while (len(assignment) < N ) or (-1 in assignment):
+					if i not in col:
+						col = np.append(col, i)
+						assignment.append(i)
+						detections = np.append(detections, [self.tracks[i].trace[-1][0,0], self.tracks[i].trace[-1][0,1]])
+						un_assigned_tracks.append(i)
+					else:
+						assignment[i] = col[i]
+						i+=1
+				
+				detections = detections.reshape(len(self.tracks), 2) # [0.1,0.1,0.2,0.2,0.3,0.3] -> [[0.1 0.1],[0.2 0.2],[0.3 0.3]]
+
+
+				""" Assigns Tracks """
+				for det_index, i in enumerate(assignment):
+					if(assignment[i] != -1):
+						#self.tracks[i].skipped_frames = 0
+						self.tracks[i].update(detections[det_index]) # Update State
+
+				"""
+				print(f"Assignments: {assignment}")
+				print(f"Detection Before Reshape: {detections}")
+				print(f"Detection After Reshape: {detections}")
+				"""
+
+				"""
+				print(f"assignments:")
+				print(f"{assignment}")
+				print(f"Detections:")
+				print(f"{detections}")
+				print(f"Length of Tracks:")
+				print(f"{len(self.tracks)}")
+				"""
+
+
+
+
+				""" Check Max Distance """
+				"""
+				for i in range(len(assignment)): #Loop for finding un_assignments
+					self.tracks[i].skipped_frames +=1
+					if assignment[i] != -1: # Check If assignments has been assigned
+						if (cost[i][assignment[i]] > self.dist_threshold): # If assigned track is exceeding to distance threshold, It will be declared to unassigned track
+							assignment[i] = -1 # Assign which track to -1
+							un_assigned_tracks.append(i) # Append to un_assigned_tracks
+				"""
+
+				""" Delete Tracks """
+				print(f"Unsigned Tracks: {un_assigned_tracks}")
+				del_tracks = []
+				for i in un_assigned_tracks:
+					self.tracks[i].skipped_frames +=1
+					print(f"Skipped Tracks {i}: {self.tracks[i].skipped_frames}")
+					if self.tracks[i].skipped_frames > self.max_frame_skipped :
+						del_tracks.append(i)
+
+				if len(del_tracks) > 0:
+					del_tracks.sort(reverse=True) # To Remove from last to front because if we dont use reverse it will cause out of length
+					self.previous_index_tracks = M # Update previous_index_tracks
+					for i in del_tracks:
+						print(f"Deleting Tracks: {i}")
+						del self.tracks[i] # Assume self.tracks = [track0, track1, track2, track3, track4, track5] del self.track[0] @ i=1 ->  [track1, track2, track3, track4, track5] 
+						del assignment[i] # Assume self.tracks = [track0, track1, track2, track3, track4, track5] del self.track[0] @ i=1 ->  [track1, track2, track3, track4, track5]
+
+
+
+
+			elif (detections.shape[0] == 0) and len(self.tracks) > 0:
+				""" Predict Assigned """
+				for j in range(len(self.tracks)):
+					detections = np.append(detections , [self.tracks[j].trace[-1][0,0], self.tracks[j].trace[-1][0,1]])
+					detections = detections.reshape(j+1, 2) # [0.1,0.1,0.2,0.2,0.3,0.3] -> [[0.1 0.1],[0.2 0.2],[0.3 0.3]]
+				N = len(self.tracks)
+				M = len(detections)
+				cost = []
+				for i in range(N):
+					diff = np.linalg.norm(self.tracks[i].prediction - detections.reshape(-1,2), axis=1) # Use Normalization 2 for compare Prediction@[k-1] and Real
+					cost.append(diff)
+
+				cost = np.array(cost)*0.1
+				row, col = linear_sum_assignment(cost) # Minimum Assignment
+				assignment = [-1]*N # Initial null assignments
+				for i in range(len(row)):
+					assignment[row[i]] = col[i] # Assigned all track
+
+				un_assigned_tracks = []
+
+				for i in range(len(assignment)): #Loop for finding un_assignments
+					self.tracks[i].skipped_frames +=1
+					if assignment[i] != -1: # Check If assignments has been assigned
+						if (cost[i][assignment[i]] > self.dist_threshold): # If assigned track is exceeding to distance threshold, It will be declared to unassigned track
+							assignment[i] = -1 # Assign which track to -1
+							un_assigned_tracks.append(i) # Append to un_assigned_tracks
+
+				
+				del_tracks = []
+				for i in range(len(self.tracks)):
+					if self.tracks[i].skipped_frames > self.max_frame_skipped :
+						del_tracks.append(i)
+
+				""" Delete All tracks """
+				if len(del_tracks) > 0:
+					del_tracks.sort(reverse=True) # To Remove from last to front because if we dont use reverse it will cause out of length
+					self.previous_index_tracks = 0 # Update previous_index_tracks
+					for i in del_tracks:
+						del self.tracks[i] # Assume self.tracks = [track0, track1, track2, track3, track4, track5] del self.track[0] @ i=1 ->  [track1, track2, track3, track4, track5] 
+						del assignment[i] # Assume self.tracks = [track0, track1, track2, track3, track4, track5] del self.track[0] @ i=1 ->  [track1, track2, track3, track4, track5]
+
+				for i in assignment:
+					if(assignment[i] != -1):
+						#self.tracks[i].skipped_frames = 0
+						self.tracks[i].update(detections[assignment[i]]) # Update State
+
